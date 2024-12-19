1d041cae7812353b9b3a658123eb8af1fa17cd8a
Silvio Waschina <s.waschina@nutrinf.uni-kiel.de>
2024-12-19 07:24:08 +0100

    first commit

---
+++ R/fba_tools.R
@@ -0,0 +1,214 @@
+#' @title Predicting auxotrophies
+#'
+#' @description Performs a FBA on the input model and on the model, where the
+#' lower bound (i.e. influx) of the compounds is set each (individually) to 0,
+#' and growth rates are compared. Works currently only for gapseq models.
+#'
+#' @param mod Model of class `ModelOrg` or a named list of these objects.
+#' @param compounds character. IDs of the model compound whose lower bound
+#' should be set to 0 in order to test its essentiality. No suffix for the
+#' external compartment is required.
+#' @param min.growth double. Minimum required growth of the original model.
+#' @param min.growth.fraction double. Minimum growth fraction of the model
+#' without the compound relative to the original model that is required to call
+#' the model prototrophic for the compound.
+#' @param multi.thread logical. Indicating if parallel processing of models is
+#' used.
+#' @param ncores integer. Number of CPUs that are used in case of parallel
+#' processing. If NULL, the number of available CPUs is detected.
+#'
+#' @return Named numeric vector with the same length and order as `compounds`.
+#' Entry of 1 indicates prototrophy and 0 auxotrophy.
+#'
+#' @import cobrar
+#' @import parallel
+#'
+#' @export
+predict_auxotrophies <- function(mod, compounds = NULL, min.growth = 0.005,
+                                 min.growth.fraction = 0.05,
+                                 multi.thread = TRUE,
+                                 ncores = NULL) {
+
+  single_mode <- FALSE
+
+  if(is.list(mod)) {
+    if(!all(unlist(lapply(mod, class)) == "ModelOrg"))
+      stop("Not all models in list are of type 'ModelOrg'")
+    if(is.null(names(mod)))
+      stop("Model list is not a named list.")
+    if(any(is.na(names(mod))))
+      stop("Not all items in the list of models are named.")
+  } else {
+    if(class(mod) != "ModelOrg")
+      stop("mod is not of class 'ModelOrg'")
+    mod <- list(mod)
+    names(mod)[1] <- mod[[1]]@mod_id
+    single_mode <- TRUE
+  }
+
+  # parallel processing?
+  n.cores <- ifelse(multi.thread, detectCores()-1, 1)
+  if(!is.null(ncores))
+    n.cores <- ncores
+  n.cores <- min(c(n.cores, length(mod)))
+  cl <- makeCluster(max(c(1,n.cores)))
+
+  if(is.null(compounds) || compounds[1] == "amino acids") {
+    compounds <- c(Ala = "cpd00035",
+                   Val = "cpd00156",
+                   Met = "cpd00060",
+                   Leu = "cpd00107",
+                   Ile = "cpd00322",
+                   Pro = "cpd00129",
+                   Trp = "cpd00065",
+                   Phe = "cpd00066",
+                   Lys = "cpd00039",
+                   Arg = "cpd00051",
+                   His = "cpd00119",
+                   Tyr = "cpd00069",
+                   Thr = "cpd00161",
+                   Glu = "cpd00023",
+                   Gln = "cpd00053",
+                   Gly = "cpd00033",
+                   Ser = "cpd00054",
+                   Cys = "cpd00084",
+                   Asp = "cpd00041",
+                   Asn = "cpd00132"
+    )
+  }
+
+  compounds <- gsub("^EX_|_.0$", "", compounds)
+  if(is.null(names(compounds)))
+    names(compounds) <- compounds
+
+  clusterExport(cl, c("compounds","min.growth","min.growth.fraction"),
+                envir = environment())
+
+  auxores <- parLapply(cl, mod, fun = worker_auxo_pred)
+  stopCluster(cl)
+
+  if(single_mode == FALSE)
+    return(auxores)
+
+  return(auxores[[1]])
+}
+
+#' Get growth rate / Value of objective function
+#'
+#' @description Uses a simple FBA to predict the models optimal value for the
+#' objective functions (most commonly the growth rate).
+#'
+#' @param mod Model of type `ModelOrg`
+#' @param multi.thread logical. Indicating if parallel processing of models is
+#' used.
+#' @param ncores integer. Number of CPUs that are used in case of parallel
+#' processing. If NULL, the number of available CPUs is detected.
+#'
+#' @return Numeric. Value of optimal value of objective function. (e.g. growth
+#' rate)
+#'
+#' @export
+get_growth <- function(mod, multi.thread = TRUE, ncores = NULL) {
+
+  single_mode <- FALSE
+
+  if(is.list(mod)) {
+    if(!all(unlist(lapply(mod, class)) == "ModelOrg"))
+      stop("Not all models in list are of type 'ModelOrg'")
+    if(is.null(names(mod)))
+      stop("Model list is not a named list.")
+    if(any(is.na(names(mod))))
+      stop("Not all items in the list of models are named.")
+  } else {
+    if(class(mod) != "ModelOrg")
+      stop("mod is not of class 'ModelOrg'")
+    mod <- list(mod)
+    names(mod)[1] <- mod[[1]]@mod_id
+    single_mode <- TRUE
+  }
+
+  # parallel processing?
+  n.cores <- ifelse(multi.thread, detectCores()-1, 1)
+  if(!is.null(ncores))
+    n.cores <- ncores
+  n.cores <- min(c(n.cores, length(mod)))
+  cl <- makeCluster(max(c(1,n.cores)))
+
+  sol <- parLapply(cl, mod, fun = worker_growth_pred)
+  stopCluster(cl)
+
+  if(single_mode == FALSE)
+    return(sol)
+
+  return(sol[[1]])
+}
+
+#------------------------------------------------------------------------------#
+# Parallel worker function
+#------------------------------------------------------------------------------#
+
+#' @import cobrar
+worker_auxo_pred <- function(x) {
+  if("cobrarCPLEX" %in% rownames(utils::installed.packages())) {
+    require(cobrarCPLEX)
+    COBRAR_SETTINGS("SOLVER","cplex")
+    okcode   <- c(1,2)
+  } else {
+    COBRAR_SETTINGS("SOLVER","glpk")
+    okcode   <- c(2,5)
+  }
+  # COBRAR_SETTINGS("SOLVER","glpk")
+  # okcode   <- c(2,5)
+  # init output
+  auxo_out <- rep(NA_real_, length(compounds))
+  names(auxo_out) <- names(compounds)
+
+  # get orig growth rate
+  m0_growth <- fba(x)@obj
+  if(m0_growth < min.growth) {
+    warning(paste0("Model ('",x@mod_id,"') has a too low or zero growth rate."))
+    return(auxo_out)
+  }
+
+  # checking auxotrophies
+  for(i in 1:length(compounds)) {
+    imet <- compounds[i]
+    ex_id <- paste0("EX_",imet,"_e0")
+    if(ex_id %in% x@react_id) {
+      mod_tmp <- changeBounds(x, ex_id, lb = 0)
+      sol_tmp <- fba(mod_tmp)
+      m1_growth <- sol_tmp@obj
+      auxo_out[i] <- m1_growth / m0_growth
+    } else {
+      auxo_out[i] <- 1
+    }
+  }
+
+  rm(mod_tmp, sol_tmp, imet, m1_growth)
+
+  auxo_out <- ifelse(auxo_out >= min.growth.fraction, 1, 0)
+
+  return(auxo_out)
+}
+
+#' @import cobrar
+worker_growth_pred <- function(x) {
+  if("cobrarCPLEX" %in% rownames(utils::installed.packages())) {
+    require(cobrarCPLEX)
+    COBRAR_SETTINGS("SOLVER","cplex")
+    okcode   <- c(1,2)
+  } else {
+    COBRAR_SETTINGS("SOLVER","glpk")
+    okcode   <- c(2,5)
+  }
+
+  sol <- fba(x)
+
+  if(sol@stat %notin% okcode)
+    warning("LP solution infeasible.")
+
+  out <- sol@obj
+  rm(sol)
+
+  return(out)
+}
---
+++ R/fetch_model_collection.R
@@ -0,0 +1,147 @@
+#' @title Fetch a collection of gapseq models
+#'
+#' @description Fetches a collection of genome-scale metabolic models
+#' reconstructed by gapseq.
+#'
+#' @param model.dir character. Path to the directory that contains the models'
+#' RDS files.
+#' @param IDs character vector that specifies which models should be retrieved
+#' from the collection. If `NULL` all models from the collection will be
+#' retrieved.
+#' @param file.type character. Select which gapseq output file should be read.
+#' One of 'model' (gap-filled model), 'draft' (draft network), 'reactions',
+#' 'pathways', 'transporters', 'medium' (predicted growth medium).
+#' @param subset integer. For testing purposes you can choose the maximum number
+#' of model files to be read.
+#' @param entries In case 'file.type' is "pathways" or "reactions", the argument
+#' can be used to limit the output to specific pathways or reactions. If `NULL`,
+#' all entries are returned.
+#' @param multi.thread logical. Indicating if parallel processing of models is
+#' used.
+#' @param ncores integer. Number of CPUs that are used in case of parallel
+#' processing. If NULL, the number of available CPUs is detected.
+#'
+#' @return A named (IDs) list of with elements of class `ModelOrg` (in case of
+#' file.type is 'model' or 'draft') or elements of class `data.table` otherwise.
+#'
+#' @import cobrar
+#'
+#' @export
+fetch_model_collection <- function(model.dir, IDs = NULL, file.type = "model",
+                                   subset = NULL, entries = NULL,
+                                   multi.thread = TRUE,
+                                   ncores = NULL) {
+  # subset argument for debugging
+  if(is.null(subset) || subset < 1)
+    subset <- Inf
+
+  #model.dir <- "/mnt/nuuk/2021/HRGM/models/"
+
+  if(file.type == "model") {
+    mod.files <- dir(model.dir, recursive = T, pattern = "\\.RDS$", full.names = T)
+    mod.files <- mod.files[!grepl("-rxnWeights\\.RDS$|-rxnXgenes\\.RDS$|-draft\\.RDS$",
+                                  mod.files)]
+
+    mod.names <- gsub("^.*/","",mod.files)
+    mod.names <- gsub("\\.RDS$","",mod.names)
+  }
+  if(file.type == "draft") {
+    mod.files <- dir(model.dir, recursive = T, pattern = "-draft\\.RDS$", full.names = T)
+
+    mod.names <- gsub("^.*/","",mod.files)
+    mod.names <- gsub("-draft\\.RDS$","",mod.names)
+  }
+  if(file.type == "reactions") {
+    mod.files <- dir(model.dir, recursive = T, pattern = "-all-Reactions\\.tbl", full.names = T)
+
+    mod.names <- gsub("^.*/","",mod.files)
+    mod.names <- gsub("-all-Reactions\\.tbl.*$","",mod.names)
+  }
+  if(file.type == "pathways") {
+    mod.files <- dir(model.dir, recursive = T, pattern = "-all-Pathways\\.tbl", full.names = T)
+
+    mod.names <- gsub("^.*/","",mod.files)
+    mod.names <- gsub("-all-Pathways\\.tbl.*$","",mod.names)
+  }
+  if(file.type == "transporters") {
+    mod.files <- dir(model.dir, recursive = T, pattern = "-Transporter\\.tbl", full.names = T)
+
+    mod.names <- gsub("^.*/","",mod.files)
+    mod.names <- gsub("-Transporter\\.tbl.*$","",mod.names)
+  }
+  if(file.type == "medium") {
+    mod.files <- dir(model.dir, recursive = T, pattern = "-medium\\.csv", full.names = T)
+
+    mod.names <- gsub("^.*/","",mod.files)
+    mod.names <- gsub("-medium\\.csv.*$","",mod.names)
+  }
+
+  if(length(mod.names) == 0)
+    stop(paste0("No model files('", file.type, "') found."))
+
+  if(any(duplicated(mod.names)))
+    stop("Duplicated model names. Please check your model path ('model.dir').")
+
+  names(mod.files) <- mod.names
+
+  if(!is.null(IDs)) {
+    tmp.ids <- IDs[IDs %in% mod.names]
+    missing.mods <- IDs[!(IDs %in% mod.names)]
+
+    if(length(missing.mods) == length(IDs)) {
+      stop(paste0("None of the IDs refer to a model file('",file.type,
+                  "') in the collection."))
+    }
+    if(length(missing.mods) > 0) {
+      n.missing <- length(missing.mods)
+      tmp.suffix <- ""
+      if(n.missing > 25) {
+        tmp.suffix <- paste0("... (",n.missing-25," IDs skipped)")
+      }
+      warning(paste0(n.missing," IDs do not have a model file('",file.type,
+                     "') in the collection:\n",
+                     paste(missing.mods, collapse = "\n"), "\n",
+                     tmp.suffix))
+    }
+
+    mod.files <- mod.files[tmp.ids]
+  }
+
+  inds <- min(c(length(mod.files), subset))
+
+  # parallel processing?
+  n.cores <- ifelse(multi.thread, detectCores()-1, 1)
+  if(!is.null(ncores))
+    n.cores <- ncores
+  n.cores <- min(c(n.cores, inds))
+  cl <- makeCluster(max(c(1,n.cores)))
+  clusterExport(cl, c("file.type","entries"), envir=environment())
+
+  if(file.type %in% c("draft","model"))
+    out <- parLapply(cl, mod.files[1:inds], fun = worker_readRDS)
+  if(file.type %in% c("reactions","pathways","transporters","medium"))
+    out <- parLapply(cl, mod.files[1:inds], fun = worker_fread)
+
+  stopCluster(cl)
+  return(out)
+}
+
+
+worker_readRDS <- function(x) {
+  return(readRDS(x))
+}
+
+#' @import data.table
+worker_fread <- function(x) {
+  res <- fread(x)
+  if(is.null(entries) | file.type %in% c("transporters","medium"))
+    return(res)
+
+  if(file.type == "pathways") {
+    entries <- c(entries, paste0("|",entries,"|"))
+    res <- res[ID %in% entries]
+  }
+  if(file.type == "reactions")
+    res <- res[rxn %in% entries]
+  return(res)
+}
